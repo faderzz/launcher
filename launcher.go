7d74fe5a997457cd5e44b1cdf7ca99356c81a5b9
faderz <58748338+faderzz@users.noreply.github.com>
2023-04-12 22:11:23 +0100

    Update launcher.go

--- launcher.go
+++ launcher.go
@@ -19,6 +19,10 @@ func main() {
 	// Window background
 	window.SetStyleSheet("background-color: #1E1E1E;")
 
+	// Custom window buttons
+	window.SetWindowFlags(core.Qt__FramelessWindowHint)
+	// Exit button
+
 	// Create widgets
 	label := widgets.NewQLabel(nil, 0)
 	pixmap := gui.NewQPixmap3("logo.png", "", core.Qt__AutoColor)
@@ -33,11 +37,30 @@ func main() {
 
 	// Microsoft Auth button
 	microsoftAuthBtn := widgets.NewQPushButton2("Sign in with Microsoft", nil)
-	microsoftAuthBtn.SetStyleSheet("background-color: #2E2E2E; color: #F0F0F0; border: 1px solid #505050; border-radius: 5px; border-radius: 15px; font: bold 14px Arial;")
-	microsoftAuthBtn.SetFixedSize2(200, 30)
+	microsoftAuthBtn.SetStyleSheet("background-color: #2E2E2E; color: #F0F0F0; border: 1px solid #505050; border-radius: 5px; border-radius: 5px; font: bold 14px Arial; margin-bottom: 10px;")
+	microsoftAuthBtn.SetFixedSize2(400, 60)
 	microsoftAuthBtn.SetIcon(gui.NewQIcon5("ms.png"))
-	microsoftAuthBtn.SetIconSize(core.NewQSize2(20, 20))
+	microsoftAuthBtn.SetIconSize(core.NewQSize2(40, 40))
 	microsoftAuthBtn.SetFlat(true)
+
+	// Hover effect
+	microsoftAuthBtn.ConnectEnterEvent(func(event *core.QEvent) {
+		// Change cursor to hand
+		microsoftAuthBtn.SetCursor(gui.NewQCursor2(core.Qt__PointingHandCursor))
+		microsoftAuthBtn.SetStyleSheet("background-color: #2E2E2E; color: #F0F0F0; border: 1px solid #505050; border-radius: 5px; border-radius: 5px; font: bold 14px Arial; margin-bottom: 10px; background-color: rgba(255, 255, 255, 0.1);")
+	})
+
+	// Hover leave effect
+	microsoftAuthBtn.ConnectLeaveEvent(func(event *core.QEvent) {
+		// Change color
+		microsoftAuthBtn.SetStyleSheet("background-color: #2E2E2E; color: #F0F0F0; border: 1px solid #505050; border-radius: 5px; border-radius: 5px; font: bold 14px Arial; margin-bottom: 10px;")
+	})
+
+	// Click function
+	microsoftAuthBtn.ConnectClicked(func(checked bool) {
+		// TODO: Implement Microsoft Auth
+		println("Microsoft Auth")
+	})
 
 	// Create a vertical layout for window
 	vLayout := widgets.NewQVBoxLayout()
