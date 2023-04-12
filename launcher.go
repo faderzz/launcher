9642c1b5e5d420c5287e134736fe84afe929db25
faderz <58748338+faderzz@users.noreply.github.com>
2023-04-12 19:59:20 +0100

    Login fields and update names

--- launcher.go
+++ launcher.go
@@ -12,17 +12,38 @@ func main() {
 
 	// Step 1: Create main window
 	window := widgets.NewQMainWindow(nil, 0)
-	window.SetWindowTitle("Minecraft Launcher")
+	window.SetWindowTitle("Project X Launcher")
 	window.SetMinimumSize2(400, 300)
 
 	// Step 2: Create widgets
-	label := widgets.NewQLabel2("Welcome to Minecraft Launcher", nil, 0)
+	label := widgets.NewQLabel2("Welcome to Project X Launcher", nil, 0)
 	label.SetAlignment(0x84) // AlignCenter
 
+	// Login Functions
+	// Contains Microsoft Auth +
+	// Mojang Auth
+
+	// Mojang Auth
+	// Email input
+	emailInput := widgets.NewQLineEdit(nil)
+	emailInput.SetPlaceholderText("Email")
+	// Password input
+	passwordInput := widgets.NewQLineEdit(nil)
+	passwordInput.SetPlaceholderText("Password")
+	passwordInput.SetEchoMode(2) // Password
+	// Login button
 	loginBtn := widgets.NewQPushButton2("Login", nil)
 	loginBtn.ConnectClicked(func(bool) {
 		// Call the login function here
 		fmt.Println("Login button clicked")
+	})
+
+	// Microsoft Auth
+	// Microsoft Auth button
+	microsoftAuthBtn := widgets.NewQPushButton2("Microsoft Auth", nil)
+	microsoftAuthBtn.ConnectClicked(func(bool) {
+		// Call the Microsoft Auth function here
+		fmt.Println("Microsoft Auth button clicked")
 	})
 
 	playBtn := widgets.NewQPushButton2("Play", nil)
